ebaaeedb378667843cdd4be55d31c732f872486c
Robert Vava <77018483+booleanvava@users.noreply.github.com>
2021-02-06 13:35:33 +0000

    Create Challenge 06 - Square(n) Sum (8 Kyu).py

---
+++ Challenge 06 - Square(n) Sum (8 Kyu).py
@@ -0,0 +1,15 @@
+"""
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+"""
+
+# Solution: 
+
+def square_sum(numbers):
+    global n
+    n = 0
+    for num in numbers: 
+        n += num * num 
+    
+    return n
